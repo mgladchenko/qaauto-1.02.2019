296b5c13f39c3bfd633fce8aeef49a669a50b87a
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2019-02-19 21:34:20 +0200

    Lesson 6

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,22 +1,41 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class LoginTests {
+    WebDriver driver;
 
-    @Test
-    public void successfulLoginTest() {
-        WebDriver driver = new ChromeDriver();
+    @BeforeMethod
+    public void beforeMethod(){
+        driver = new ChromeDriver();
         driver.get("https://www.linkedin.com/");
+    }
 
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+
+    @DataProvider
+    public Object[][] validData() {
+        return new Object[][]{
+                { "linkedin.tst.yanina@gmail.com", "Test123!" },
+                { "linkedin.TST.yanina@gmail.com", "Test123!" },
+                { " linkedin.tst.yanina@gmail.com ", "Test123!" }
+        };
+    }
+
+    @Test(dataProvider = "validData")
+    public void successfulLoginTest(String userEmail, String userPassword) {
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(),
                 "Landing page is not loaded.");
 
-        landingPage.login("linkedin.tst.yanina@gmail.com", "Test123!");
+        landingPage.login(userEmail, userPassword);
 
         HomePage homePage = new HomePage(driver);
         Assert.assertTrue(homePage.isPageLoaded(),
@@ -25,9 +44,6 @@ public class LoginTests {
 
     @Test
     public void negativeLoginReturnedToLandingTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
-
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(),
                 "Landing page is not loaded.");
@@ -39,9 +55,6 @@ public class LoginTests {
 
     @Test
     public void negativeLoginReturnedToLoginSubmitTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
-
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(),
                 "Landing page is not loaded.");
