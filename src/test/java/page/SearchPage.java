319709adf0907532230250ee3c40e84851e56e5f
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2019-03-01 21:38:55 +0200

    Lesson 9

--- src/test/java/page/SearchPage.java
+++ src/test/java/page/SearchPage.java
@@ -1,15 +1,22 @@
 package page;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchPage {
     WebDriver driver;
 
     @FindBy(xpath = "//h3[contains(@class, 'search-results__total')]")
-    private WebElement searchResultsTotal;
+    private WebElement searchResultTotal;
+
+    @FindBy(xpath = "//li[contains(@class, 'search-result ')]")
+    private List<WebElement> searchResultElements;
 
     public SearchPage(WebDriver driver) {
         this.driver = driver;
@@ -17,8 +24,23 @@ public class SearchPage {
     }
 
     public boolean isPageLoaded() {
-        return searchResultsTotal.isDisplayed()
+        return searchResultTotal.isDisplayed()
                 && driver.getCurrentUrl().contains("/search/results")
                 && driver.getTitle().contains("| Search | LinkedIn");
+    }
+
+    public int getSearchResultCount() {
+        return searchResultElements.size();
+    }
+
+    public List<String> getSearchResultsList() {
+        List<String> searchResultStringsList = new ArrayList<String>();
+        for (WebElement searchResultElement : searchResultElements) {
+            ((JavascriptExecutor) driver).executeScript(
+                            "arguments[0].scrollIntoView(true);", searchResultElement);
+            String searchResultString = searchResultElement.getText();
+            searchResultStringsList.add(searchResultString);
+        }
+        return searchResultStringsList;
     }
 }
--- src/test/java/test/SearchTests.java
+++ src/test/java/test/SearchTests.java
@@ -5,8 +5,24 @@ import org.testng.annotations.Test;
 import page.HomePage;
 import page.SearchPage;
 
+import java.util.List;
+
 public class SearchTests extends BaseTest {
 
+    /**
+     * Verify basic search functionality.
+     *
+     * Scenario:
+     * - Open new Browser
+     * - Navigate to http://www.linkedin.com
+     * - Verify that Landing page is loaded
+     * - Login with valid credentials
+     * - Verify that Home page is loaded
+     * - Search for "HR" search term
+     * - Verify that Search page is loaded
+     * - Verify that numbers of search results is 10
+     * - Verify that each search result contains search term
+     */
     @Test
     public void basicSearchTest() {
         String userEmail = "linkedin.tst.yanina@gmail.com";
@@ -24,6 +40,15 @@ public class SearchTests extends BaseTest {
         Assert.assertTrue(searchPage.isPageLoaded(),
                 "Search page did not load.");
 
+        Assert.assertEquals(searchPage.getSearchResultCount(), 10,
+                "Search results count is wrong.");
+
+        List<String> searchResultsList = searchPage.getSearchResultsList();
+
+        for (String searchResult: searchResultsList) {
+            Assert.assertTrue(searchResult.contains(searchTerm),
+                    "SearchTerm "+searchTerm+" not found in:\n"+searchResult);
+        }
 
     }
 
